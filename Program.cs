1447322b99a15d0c20a896964bfb799164e06960
Elijah Rogers <ejrogersemail@gmail.com>
2024-11-18 10:05:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment3_1_3
+{
+    internal class Program
+    {
+        // Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence to amount of spaces in the sentence");
+            Console.Write("Here: ");
+            string sentence = Console.ReadLine();
+            Console.WriteLine($"\nSentence: { sentence }");
+            Console.WriteLine($"\nSpaces: { CountSpacesInSentence(sentence) }");
+
+        }
+
+        // Counts the spaces within a provided sentence and returns the number of spaces
+        static int CountSpacesInSentence(string sentence)
+        {
+            int count = 0;
+            foreach (char c in sentence)
+            {
+                if (c == ' ')
+                {
+                    count++;
+                }
+
+            }
+            return count;
+            
+        }
+    }
+}
